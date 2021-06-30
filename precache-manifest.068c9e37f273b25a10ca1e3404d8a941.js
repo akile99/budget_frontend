a7ac3adeeac2928d04af9c709ac82f615ff828a4
akile <akile99@gmail.com>
2021-06-30 12:07:16 -0400

    Updates

---
+++ precache-manifest.068c9e37f273b25a10ca1e3404d8a941.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "c89daeb276e23949ba9c",
+    "url": "/budget_frontend/static/css/main.3c4aed50.chunk.css"
+  },
+  {
+    "revision": "c89daeb276e23949ba9c",
+    "url": "/budget_frontend/static/js/main.22e9b0d6.chunk.js"
+  },
+  {
+    "revision": "39a060c072a6c331c1b3",
+    "url": "/budget_frontend/static/js/runtime~main.8d9defb4.js"
+  },
+  {
+    "revision": "8ef0a7b5ee7ce02cda66",
+    "url": "/budget_frontend/static/css/2.3a4758f2.chunk.css"
+  },
+  {
+    "revision": "8ef0a7b5ee7ce02cda66",
+    "url": "/budget_frontend/static/js/2.36dada37.chunk.js"
+  },
+  {
+    "revision": "f0e1b6339eb74f03d9dc",
+    "url": "/budget_frontend/static/js/3.787719fa.chunk.js"
+  },
+  {
+    "revision": "a55ca56e444f84177dbc8009a125c914",
+    "url": "/budget_frontend/index.html"
+  }
+];
