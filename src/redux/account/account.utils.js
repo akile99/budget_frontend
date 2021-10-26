f2dbe31e7d4b0ca640acb73a266e1fa60a23233e
akile99 <akile99@gmail.com>
2021-10-26 14:01:46 -0400

    fixed the balance to only show two decimal places

--- src/redux/account/account.utils.js
+++ src/redux/account/account.utils.js
@@ -6,6 +6,8 @@ export const updateAccountBalance = (currentAccount, newBalance) => {
   console.log(`new balance ${currentAccount.balance}`);
   return {
     ...currentAccount,
-    balance: parseFloat(currentAccount.balance) + parseFloat(newBalance),
+    balance: (
+      parseFloat(currentAccount.balance) + parseFloat(newBalance)
+    ).toFixed(2),
   };
 };
