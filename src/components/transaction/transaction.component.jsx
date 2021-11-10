9e14a0b4280c2fadb7333d0b49ad061e76cf9c61
akile99 <akile99@gmail.com>
2021-11-09 19:14:12 -0500

    added js as css to transaction componet. removed old scss styles from transaction

--- src/components/transaction/transaction.component.jsx
+++ src/components/transaction/transaction.component.jsx
@@ -1,30 +1,34 @@
 import React from "react";
 
-import "./transaction.styles.scss";
+import {
+  TransactionContainer,
+  DateContainer,
+  VendorContainer,
+  StatusContainer,
+  Dollar,
+  AmountContainer,
+  CategoryContainer,
+} from "./transaction.styles";
 
 const Transaction = ({ transaction }) => {
   const { date, vendor, amount, status, category } = transaction;
 
   function formatDate(date) {
     const d = new Date(date);
-
     const year = d.getFullYear();
     const month = d.getMonth() + 1;
-    const day = d.getDate() + 1;
-
+    const day = d.getDate();
     return month + "-" + day + "-" + year;
   }
   return (
-    <div className="outline">
-      <div className="flex justify-left">
-        <p className="date transaction">{formatDate(date)}</p>
-        <p className="vendor1 transaction">{vendor}</p>
-        <p className="dollar transaction">$</p>
-        <p className="amount transaction">{amount}</p>
-        <p className="status">{status} </p>
-        <p className="category transaction">{category}</p>
-      </div>
-    </div>
+    <TransactionContainer>
+      <DateContainer>{formatDate(date)}</DateContainer>
+      <VendorContainer>{vendor}</VendorContainer>
+      <Dollar>$</Dollar>
+      <AmountContainer>{amount}</AmountContainer>
+      <StatusContainer>{status} </StatusContainer>
+      <CategoryContainer>{category}</CategoryContainer>
+    </TransactionContainer>
   );
 };
 
